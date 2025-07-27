ea8c74abcb7e356b63c46ab5d2f1ef1a80de7840
AnjanaK31 <anjanakrishnan787878@gmail.com>
2025-07-27 23:47:45 +0530

    weather structure + basics

---
+++ Lab2/script.js
@@ -0,0 +1,33 @@
+async function getWeather() {
+  const city = document.getElementById('cityInput').value;
+  const apiKey = '77422c757a8458eef9160062524b1997'; 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const errorText = document.getElementById('error');
+  const card = document.getElementById('weatherCard');
+  errorText.textContent = "";
+  card.classList.add("hidden");
+
+  if (!city) {
+    errorText.textContent = "Please select a city.";
+    return;
+  }
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("City not found");
+    const data = await response.json();
+
+    document.getElementById('city').textContent = data.name;
+    document.getElementById('icon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.getElementById('icon').alt = data.weather[0].description;
+    document.getElementById('description').textContent = data.weather[0].description;
+    document.getElementById('temp').textContent = `Temperature: ${data.main.temp}°C`;
+    document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
+    document.getElementById('wind').textContent = `Wind Speed: ${data.wind.speed} km/h`;
+
+    card.classList.remove("hidden");
+  } catch (error) {
+    errorText.textContent = error.message;
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Lab exercises corresponding to the course CS3005: Web Technologies
 
-Checkout my Resume here: https://anjanak31.github.io/WebTech/Lab1/Resume and my CV: https://anjanak31.github.io/WebTech/Lab1/CV
