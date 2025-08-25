ec7ce28f20720035e161ede45084afb4e80cbfc7
AnjanaK31 <anjanakrishnan787878@gmail.com>
2025-08-25 22:40:42 +0530

    lab-6 rough edit

---
+++ Lab6/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Heading from '../components/Heading'
+import Timer from '../components/Timer'
+
+function App() {
+
+
+  return (
+    <>
+      <Heading></Heading>
+      <Timer></Timer>
+    </>
+  )
+}
+
+export default App
