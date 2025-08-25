dee618eb4cbaa127bedae7f315e2d8565cad75a4
AnjanaK31 <anjanakrishnan787878@gmail.com>
2025-08-25 22:40:42 +0530

    lab-6 rough edit

---
+++ Lab6/src/App.jsx
@@ -0,0 +1,14 @@
+import Heading from './components/Heading'
+import TimerInput from './components/TimerInput'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <Heading></Heading>
+      <TimerInput></TimerInput>
+    </>
+  )
+}
+
+export default App
---
+++ Lab6/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Heading = () => {
+
+  return (
+    <>
+        <h1>Count Down Timer</h1>
+    </>
+  )
+}
+
+export default Heading
